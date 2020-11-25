0fc3292f81038592e91af6de1109a10f9b97c7d5
NZGreenCode <71475638+NZGreenCode@users.noreply.github.com>
2020-11-25 19:21:08 +1300

    Working Code

--- CRUD2020/Program.cs
+++ CRUD2020/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Azure.Identity;
 
 namespace CRUD2020
 {
@@ -19,12 +12,7 @@ namespace CRUD2020
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-    _ = config.AddAzureKeyVault(
-    new Uri(Environment.GetEnvironmentVariable("VaultUri")),
-    new DefaultAzureCredential());
-})
+
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
